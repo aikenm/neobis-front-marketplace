8b25c9bcd01093baae1e6393852347a8d64326f4
aikenm <a.mnsbkv@gmail.com>
2023-09-25 15:33:45 +0600

    Finished header of the main page

--- src/components/profile_components/ProfileInfo.jsx
+++ src/components/profile_components/ProfileInfo.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import arrow from '../../images/arrow.svg'
 
 function ProfileInfo() {
     const user = useSelector(state => state.user);
 
     return (
         <div>
+            <Link to="/main" className='back-button'>
+                <img src={arrow} alt="back" className='arrow-icon'/>
+                <span className='arrow-text'>Назад</span>
+            </Link>
             <h2>Profile Info</h2>
             <p>Login: {user.login}</p>
             <p>Email: {user.email}</p>
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,14 +1,38 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import '../styles/image_block.css';
 import '../styles/forms.css';
 import '../styles/core.css';
+import avatar from '../images/avatar.svg';
+import miniLogo from '../images/mini-logo.svg';
 
 
 function MainPage() {
+    const user = useSelector(state => state.user);
     
     return (
         <div className='main-page'>
-            <h1>Main</h1>
+            <div className='header'>
+                <div className='header-logo'>
+                    <img src={miniLogo} alt='logo' className='main-logo-icon' />
+                    <span className='header-logo-text'>MOBI MARKET</span>
+                </div>
+                <button 
+                    className='form-button add-item-button' >
+                        Подать обьявление
+                </button>
+                <Link to="/profile" className='main-profile-button'>
+                    <div className='profile-info'>
+                        <span className='user-login'>{user.login}</span>
+                        <span className='user-email'>{user.email}</span>
+                    </div>
+                    <img src={avatar} alt='avatar' className='avatar'/>
+                </Link>
+            </div>
+            <div className='items-section'>
+                <h1>sec</h1>
+            </div>
         </div>
     );
 }
